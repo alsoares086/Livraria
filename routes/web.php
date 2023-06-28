392203cbda60e3a5524c1928f6a920f3e78eab43
alsoares086 <117699313+alsoares086@users.noreply.github.com>
2023-06-28 17:07:48 -0500

    agrupando as rotas

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivroController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome');});*/
 
+/*
 Route::get('/livros', [LivroController::class, 'index']);
 Route::get('/livros/create', [LivroController::class, 'create']);
 Route::post('/livros', [LivroController::class, 'store']);
-Route::get('/livros/mostrar', [LivroController::class, 'show']);
+Route::get('/livros/mostrar', [LivroController::class, 'show']);*/
+
+Route::prefix('livros')->group(function () {
+    Route::get('/', [LivroController::class, 'index'])->name('livros.index');
+    Route::get('/create', [LivroController::class, 'create'])->name('livros.create');
+    Route::post('/', [LivroController::class, 'store'])->name('livros.store');
+    Route::get('/mostrar', [LivroController::class, 'show'])->name('livros.show');
+});
+
+?>
+
 
