a6dedaa77962333527ec6fddfb2cb8710cc0a795
alsoares086 <117699313+alsoares086@users.noreply.github.com>
2023-06-27 22:10:54 -0500

    alterando resolução da capa do livro

--- resources/views/livros/mostrar.blade.php
+++ resources/views/livros/mostrar.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Lista de Livros</title>
 </head>
 <body>
@@ -16,7 +17,7 @@
             <p>Ano: {{$livro->ano_publicacao}}</p>
             <p>Gênero: {{$livro->genero->nome}}</p>
             @if($livro->capa)
-                <img src="/img/capas/{{$livro->capa}}" alt="Capa do Livro">
+                <img src="/img/capas/{{$livro->capa}}" alt="Capa do Livro" class="img-capa">
             @else
                 <p>Capa indisponível</p>
             @endif
